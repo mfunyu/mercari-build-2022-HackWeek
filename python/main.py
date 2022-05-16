a84e2e15deba950d290cdd991ac1cd9864dfad78
大矢みなみ <minami@CORE7.local>
2022-05-16 22:37:57 +0900

    Finished step3 task9

--- python/main.py
+++ python/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "image"
 origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
 app.add_middleware(
